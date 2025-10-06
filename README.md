81de78a95210884fb9c08286bc905f120142245f
Aiman Fazal <twistofaiman@gmail.com>
2025-10-06 10:58:44 +0530

    added different categories of quotes which can be selected by the user input

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# mini-api-project
+## mini-api-project
+
 This is a basic set of programs that helped me to get used to working of APIs in Python.
+Before using this program, make sure you generate your API Key from the followoing websites:
+
+*   https://dailyquotes.dev/
+
+Add your API keys in the .env file of your working directory.
--- quotes.py
+++ quotes.py
@@ -4,8 +4,17 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+categories = ['quotes', 'anime', 'movie', 'dev', 'motivational', 'self_improvement', 'game', 'book', 'poetry', 'mentalhealth']
+print("Welcome to Random Quote Generator Program!")
+
+for cat in range(len(categories)):
+    print(f"{cat + 1} = {categories[cat]}")
+
+i = int(input("Select the category of the quote211qqew: ")) - 1
+
 api_key = os.getenv('API_KEY')
-url = "https://api.dailyquotes.dev/api/quotes/motivational"
+
+url = f"https://api.dailyquotes.dev/api/quotes/{categories[i]}"
 headers = {
     "Authorization": f"Bearer {api_key}",
     "Content-Type": "application/json"
